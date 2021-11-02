418245312e0d9d5b3f18f7a4f167c9b76190370e
OkanEkici <83089376+OkanEkici@users.noreply.github.com>
2021-11-02 11:13:41 +0100

    Add files via upload

---
+++ app/Http/Controllers/TeamController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Tenant;
+use App\Jobs\TenantDatabase;
+use App\Services\TenantManager;
+use Illuminate\Support\Facades\Auth;
+
+class TeamController extends Controller
+{
+    public function __construct() {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tenants = Auth::user()->tenants()->get();
+
+        return view('user.team.index', ['tenants' => $tenants]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('user.team.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $tenant = new Tenant;
+        $tenant->subdomain = $request->get('subdomain');
+        $tenant->name = $request->get('name');
+        $tenant->user()->associate($request->user());
+
+        $tenant->save();
+
+        TenantDatabase::dispatch($tenant, app(TenantManager::class));
+        return 'Success';
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TenantKeysController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tenant_Keys;
+use Illuminate\Http\Request;
+
+class TenantKeysController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tenant_Keys  $tenant_Keys
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tenant_Keys $tenant_Keys)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tenant_Keys  $tenant_Keys
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tenant_Keys $tenant_Keys)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tenant_Keys  $tenant_Keys
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tenant_Keys $tenant_Keys)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tenant_Keys  $tenant_Keys
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tenant_Keys $tenant_Keys)
+    {
+        //
+    }
+}
