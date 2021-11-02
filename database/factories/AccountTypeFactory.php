ad49fbf8c512c77f36948e56beca3245b41b6aff
OkanEkici <83089376+OkanEkici@users.noreply.github.com>
2021-11-02 11:22:33 +0100

    Create AccountTypeFactory.php

---
+++ database/factories/AccountTypeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\AccountType;
+use Faker\Generator as Faker;
+
+$factory->define(AccountType::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
